4277610f3875da289c8c54b6c4377ffe99292cbe
akmal77 <akmal2@mail.ru>
2014-05-12 20:03:42 +0500

    add simple spring ver

---
+++ src/main/java/uz/micros/jstore/controller/IndexController.java
@@ -0,0 +1,26 @@
+package uz.micros.jstore.controller;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by java on 12.05.14.
+ */
+@Controller
+public class IndexController {
+
+    @ResponseBody
+     @RequestMapping(value = "/", method = RequestMethod.GET)
+     public String showIndex(){
+        return "<h2>Hello World spring!!!</h2>";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/fe", method = RequestMethod.GET)
+    public String showIndex1(){
+        return "<h2>Hello World spring 222222!!!</h2>";
+    }
+}
