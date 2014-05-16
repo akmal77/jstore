0ecc5a30a16f118e9fceb7c145fc2e05df7c8f7e
akmal77 <akmal2@mail.ru>
2014-05-16 19:57:36 +0500

    get_post

--- src/main/java/uz/micros/jstore/controller/IndexController.java
+++ src/main/java/uz/micros/jstore/controller/IndexController.java
@@ -2,9 +2,13 @@ package uz.micros.jstore.controller;
 
 import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import uz.micros.jstore.entity.Author;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by java on 12.05.14.
@@ -22,7 +26,53 @@ public class IndexController {
 
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String showIndex1(){
-        return "home";
+    public ModelAndView showIndex1(HttpSession session){
+
+        Author a = new Author();
+        a.setName("Borya");
+
+        List<Author> list;
+        if (session.getAttribute("authors") == null){
+
+            list = new ArrayList<Author>();
+            list.add(a);
+            list.add(a);
+            list.add(a);
+            session.setAttribute("authors", list);
+        }else{
+            list = (List<Author>) session.getAttribute("authors");
+        }
+
+
+
+        return new ModelAndView("home").addObject("name", "Muzaffar").addObject("author", a).addObject("authors", list)
+                .addObject("newAuthor", new Author());
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+
+    public String save(@RequestParam("title") String name, HttpSession session){
+
+        List<Author> list = (List<Author>) session.getAttribute("authors");
+
+        Author a = new Author();
+        a.setName(name);
+        list.add(a);
+
+        return "redirect:/home";
+
+    }
+
+
+    @RequestMapping(value = "/save2", method = RequestMethod.POST)
+
+    public String save2(@ModelAttribute Author newAuthor, HttpSession session){
+
+        List<Author> list = (List<Author>) session.getAttribute("authors");
+
+        list.add(newAuthor);
+
+        return "redirect:/home";
+
     }
 }
